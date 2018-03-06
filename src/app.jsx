6a38e48f9521d65ef047c01437a4715c6dd6cbfc
Alisher Muhamed <alisher.muhamed@yandex.ru>
2018-03-06 16:44:49 +0300

    UPDATE package.json, babel config, DELETE unnecessary files

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const store = configureStore();
-
-ReactDOM.render(
-  <h1>Stack Application</h1>,
-  document.getElementById('root'),
-);
--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as RadioBox } from './RadioBox';
-export { default as SelectBox } from './SelectBox';
-export { default as TextBox } from './TextBox';
