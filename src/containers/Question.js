e37b8cba7c3a30d7f457594aa898e531c90abac4
Alisher Muhamed <alisher.muhamed@yandex.ru>
2018-03-09 03:11:02 +0300

    UPDATE question props

--- src/containers/Question.js
+++ src/containers/Question.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux';
 import QuestionComponent from '../components/Question';
 
-// where to put match to pass an id?
+// Write props properly
 const mapStateToProps = ({ question }, ownProps) => {
   return {
-    questionId: ownProps.match.params.id,
-    title: question.title,
-    score: question.score,
-    body: question.body,
-    owner: question.owner,
-    creationDate: question.creationDate,
-    answers: question.answers
+    question_id: ownProps.match.params.id,
+    isFetching: question.isFetching
   };
 };
 
