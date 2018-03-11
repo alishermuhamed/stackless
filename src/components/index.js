79c5057329df41b74c7f0577be4f11a276d1a35d
Alisher Muhamed <alisher.muhamed@yandex.ru>
2018-03-11 15:17:46 +0300

    DELETE embedded components

--- src/components/index.js
+++ src/components/index.js
@@ -2,20 +2,14 @@ import { SearchBar } from './SearchBar';
 import { FilterBar } from './FilterBar';
 import { SideBar } from './SideBar';
 import { List } from './List';
-import { ListItem } from './ListItem';
-import { Owner } from './Owner';
 import { Question } from './Question';
-import { Post } from './Post';
 import { User } from './User';
 
 export { default as SearchBar } from './SearchBar';
 export { default as FilterBar } from './FilterBar';
 export { default as SideBar } from './SideBar';
 export { default as List } from './List';
-export { default as ListItem } from './ListItem';
-export { default as Owner } from './Owner';
 export { default as Question } from './Question';
-export { default as Post } from './Post';
 export { default as User } from './User';
 
 export default {
@@ -23,9 +17,6 @@ export default {
   FilterBar,
   SideBar,
   List,
-  ListItem,
-  Owner,
   Question,
-  Post,
   User
 };
