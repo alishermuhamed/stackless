a0471929fb9f12a079bf8d13724eb6d7c9a10403
Ewgeny Filippow <filippow.ewgen@yandex.ru>
2018-03-19 17:12:38 +0300

    CHANGE position for author and tags

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -21,6 +21,7 @@ const Post = props => {
           >
             {props.owner.displayName}
           </Link>
+          <div></div>
           {parseUnixTimeStamp(props.creationDate)}
         </span>
       </div>
